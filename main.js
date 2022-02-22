10bb2bd50fa2fb3048eeb5a63ed7267940addea8
Vladimir Kalnitsky <klntsky@gmail.com>
2022-02-22 22:43:43 +0300

    New version

--- main.js
+++ main.js
@@ -1,5 +1,92 @@
 var player;
 
+// From can-autoplay.js
+
+const AUDIO = new Blob([new Uint8Array([255, 227, 24, 196, 0, 0, 0, 3, 72, 1, 64, 0, 0, 4, 132, 16, 31, 227, 192, 225, 76, 255, 67, 12, 255, 221, 27, 255, 228, 97, 73, 63, 255, 195, 131, 69, 192, 232, 223, 255, 255, 207, 102, 239, 255, 255, 255, 101, 158, 206, 70, 20, 59, 255, 254, 95, 70, 149, 66, 4, 16, 128, 0, 2, 2, 32, 240, 138, 255, 36, 106, 183, 255, 227, 24, 196, 59, 11, 34, 62, 80, 49, 135, 40, 0, 253, 29, 191, 209, 200, 141, 71, 7, 255, 252, 152, 74, 15, 130, 33, 185, 6, 63, 255, 252, 195, 70, 203, 86, 53, 15, 255, 255, 247, 103, 76, 121, 64, 32, 47, 255, 34, 227, 194, 209, 138, 76, 65, 77, 69, 51, 46, 57, 55, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 255, 227, 24, 196, 73, 13, 153, 210, 100, 81, 135, 56, 0, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170])], {type: 'audio/mpeg'});
+
+/**
+ * @type {Blob}
+ */
+const VIDEO = new Blob([new Uint8Array([0, 0, 0, 28, 102, 116, 121, 112, 105, 115, 111, 109, 0, 0, 2, 0, 105, 115, 111, 109, 105, 115, 111, 50, 109, 112, 52, 49, 0, 0, 0, 8, 102, 114, 101, 101, 0, 0, 2, 239, 109, 100, 97, 116, 33, 16, 5, 32, 164, 27, 255, 192, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 55, 167, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 112, 33, 16, 5, 32, 164, 27, 255, 192, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 55, 167, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 112, 0, 0, 2, 194, 109, 111, 111, 118, 0, 0, 0, 108, 109, 118, 104, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 232, 0, 0, 0, 47, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 1, 236, 116, 114, 97, 107, 0, 0, 0, 92, 116, 107, 104, 100, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 36, 101, 100, 116, 115, 0, 0, 0, 28, 101, 108, 115, 116, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 47, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 100, 109, 100, 105, 97, 0, 0, 0, 32, 109, 100, 104, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 172, 68, 0, 0, 8, 0, 85, 196, 0, 0, 0, 0, 0, 45, 104, 100, 108, 114, 0, 0, 0, 0, 0, 0, 0, 0, 115, 111, 117, 110, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 83, 111, 117, 110, 100, 72, 97, 110, 100, 108, 101, 114, 0, 0, 0, 1, 15, 109, 105, 110, 102, 0, 0, 0, 16, 115, 109, 104, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 36, 100, 105, 110, 102, 0, 0, 0, 28, 100, 114, 101, 102, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 12, 117, 114, 108, 32, 0, 0, 0, 1, 0, 0, 0, 211, 115, 116, 98, 108, 0, 0, 0, 103, 115, 116, 115, 100, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 87, 109, 112, 52, 97, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 16, 0, 0, 0, 0, 172, 68, 0, 0, 0, 0, 0, 51, 101, 115, 100, 115, 0, 0, 0, 0, 3, 128, 128, 128, 34, 0, 2, 0, 4, 128, 128, 128, 20, 64, 21, 0, 0, 0, 0, 1, 244, 0, 0, 1, 243, 249, 5, 128, 128, 128, 2, 18, 16, 6, 128, 128, 128, 1, 2, 0, 0, 0, 24, 115, 116, 116, 115, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 28, 115, 116, 115, 99, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 28, 115, 116, 115, 122, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1, 115, 0, 0, 1, 116, 0, 0, 0, 20, 115, 116, 99, 111, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 44, 0, 0, 0, 98, 117, 100, 116, 97, 0, 0, 0, 90, 109, 101, 116, 97, 0, 0, 0, 0, 0, 0, 0, 33, 104, 100, 108, 114, 0, 0, 0, 0, 0, 0, 0, 0, 109, 100, 105, 114, 97, 112, 112, 108, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 105, 108, 115, 116, 0, 0, 0, 37, 169, 116, 111, 111, 0, 0, 0, 29, 100, 97, 116, 97, 0, 0, 0, 1, 0, 0, 0, 0, 76, 97, 118, 102, 53, 54, 46, 52, 48, 46, 49, 48, 49])], {type: 'video/mp4'});
+
+
+const APIKey = 'AIzaSyAqQ0H76XHmXGYwyK8C9DILPgSbBqgPk_k';
+
+function setupDefaultValues (options) {
+  return Object.assign({
+    muted: false,
+    timeout: 250,
+    inline: false
+  }, options)
+}
+
+function startPlayback ({muted, timeout, inline}, elementCallback) {
+  let {element, source} = elementCallback()
+  let playResult
+  let timeoutId
+  let sendOutput
+
+  element.muted = muted
+  if (muted === true) {
+    element.setAttribute('muted', 'muted')
+  }
+  // indicates that the video is to be played "inline",
+  // that is within the element's playback area.
+  if (inline === true) {
+    element.setAttribute('playsinline', 'playsinline')
+  }
+
+  element.src = source
+
+  return new Promise(resolve => {
+    playResult = element.play()
+    timeoutId = setTimeout(() => {
+      sendOutput(false, new Error(`Timeout ${timeout} ms has been reached`))
+    }, timeout)
+    sendOutput = (result, error = null) => {
+      // Clean up to avoid MediaElementLeak
+      element.remove()
+      element.srcObject = null
+
+      clearTimeout(timeoutId)
+      resolve({result, error})
+    }
+
+    if (playResult !== undefined) {
+      playResult
+        .then(() => sendOutput(true))
+        .catch(playError => sendOutput(false, playError))
+    } else {
+      sendOutput(true)
+    }
+  })
+}
+
+//
+// API
+//
+
+function video (options) {
+  options = setupDefaultValues(options)
+  return startPlayback(options, () => {
+    return {
+      element: document.createElement('video'),
+      source: URL.createObjectURL(VIDEO)
+    }
+  })
+}
+
+function audio (options) {
+  options = setupDefaultValues(options)
+  return startPlayback(options, () => {
+    return {
+      element: document.createElement('audio'),
+      source: URL.createObjectURL(AUDIO)
+    }
+  })
+}
+
+
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         height: '390',
@@ -47,7 +134,7 @@ function parseHash () {
         throw "Invalid format!";
 }
 
-const model = { players: {}, count: 0 };
+const model = { players: {}, count: 0, readyCount: 0 };
 
 const players = model.players;
 
@@ -65,11 +152,11 @@ function createPlayer(ix, videoId, volume) {
             'playsinline': 1
         },
         enablejsapi: 1,
-        autoplay: 1,
+        autoplay: 0,
         loop: 1,
         events: {
             'onReady': event => {
-                event.target.playVideo();
+                model.readyCount++;
                 player.setVolume(volume);
                 player.setLoop(true);
             }
@@ -125,7 +212,7 @@ const addField = (ix, videoId, volume) => {
     row.appendChild(volumeInput);
     row.appendChild(delInput);
     row.className = 'entry';
-    row.id = ix;
+    row.id = 'row-' + ix;
     delInput.addEventListener('click', _ => {
         row.remove();
         players[ix].destroy();
@@ -142,8 +229,11 @@ function putFields (videos) {
     });
 }
 
+// const parseId = link =>
+//       link.split('=')[1] || '';
+
 const parseId = link =>
-      link.split('=')[1] || '';
+      link.match(/v=.{11}/g)[0].substr(2);
 
 function getDataFromFields() {
     const entries = document.querySelector('#fields').getElementsByClassName('entry');
@@ -158,13 +248,40 @@ function getDataFromFields() {
             linkEl.style.borderColor = 'unset';
         }
         const volume = entry.getElementsByClassName('volume')[0].value;
-        res.push([parseInt(entry.id), parseId(link), parseInt(volume)]);
+        res.push([parseInt(entry.id.substr(4)), parseId(link), parseInt(volume)]);
     }
     return res;
 };
 
 function getHashForVideos (videos) {
     return videos.map(([_, videoId, volume]) => videoId + '.' + volume).join(',');
+}
+
+async function loadVideoTitles () {
+    try {
+    for (let playerId in model.players) {
+        if (model.players.hasOwnProperty(playerId)) {
+            const player = model.players[playerId];
+            const videoUrl = player.getVideoUrl();
+            if (!videoUrl)
+                continue;
+            const videoId = parseId(player.getVideoUrl());
+            const title = await loadVideoTitle(videoId);
+            const container = document.getElementById('row-'+playerId);
+            let titleElement = container.querySelectorAll('.video-title')[0];
+
+            if (!titleElement) {
+                titleElement = document.createElement('span');
+                titleElement.className = 'video-title';
+                container.appendChild(titleElement);
+            }
+
+            titleElement.textContent = title;
+        }
+    }
+    } catch (e) {
+        console.log('e', e);
+    }
 }
 
 function applyChanges () {
@@ -176,7 +293,7 @@ function applyChanges () {
     const updatedHandle = document.getElementById('updated-handle');
     updatedHandle.style.display = 'inline';
     setTimeout(() => updatedHandle.style.display = 'none', 1000);
-
+    setTimeout(loadVideoTitles, 1000);
 }
 
 function setHandlers () {
@@ -184,16 +301,86 @@ function setHandlers () {
         addField(model.count, '', 100);
         model.count++;
     });
+    document.querySelector('#play-button').addEventListener('click', startAllPlayersNow);
+    document.querySelector('#pause-button').addEventListener('click', pauseAllPlayers);
 }
 
-function init () {
+function startAllPlayers () {
+    let tid = setInterval(() => {
+        if (model.readyCount == model.count) {
+            for (let playerKey in players) {
+                if (players.hasOwnProperty(playerKey)) {
+                    players[playerKey].playVideo();
+                }
+            }
+            clearInterval(tid);
+        }
+    }, 10);
+}
+
+function awaitAllPlayersReady () {
+    return new Promise((resolve, _reject) => {
+        if (model.readyCount == model.count) {
+            resolve();
+            return;
+        }
+
+        let tid = setInterval(() => {
+            if (model.readyCount == model.count) {
+                clearInterval(tid);
+                resolve();
+            }
+        }, 10);
+    });
+}
+
+function startAllPlayersNow () {
+    for (let playerKey in players) {
+        if (players.hasOwnProperty(playerKey)) {
+            players[playerKey].playVideo();
+        }
+    }
+}
+
+function pauseAllPlayers () {
+    for (let playerKey in players) {
+        if (players.hasOwnProperty(playerKey)) {
+            players[playerKey].pauseVideo();
+        }
+    }
+}
+
+async function init () {
     const videos = parseHash();
     model.count = videos.length;
     putPlayers(videos);
     putFields(videos);
     setHandlers();
     document.getElementById('mix-link').href = document.location.href;
+    const canPlay = (await audio()) && (await video());
+    if (canPlay) {
+        startAllPlayers();
+    }
+    await awaitAllPlayersReady();
+    loadVideoTitles();
 }
+
+function memoize(fn) {
+    const cache = {};
+    return async function() {
+        const args = JSON.stringify(arguments);
+        cache[args] = cache[args] || fn.apply(undefined, arguments);
+        return cache[args];
+    };
+}
+
+const loadVideoTitle = memoize(async (videoId) => {
+    const url = 'https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id=' + videoId + '&key=' + APIKey;
+    const response = await fetch(url);
+    const json = await response.json();
+    console.log(videoId, json);
+    return json.items[0].snippet.title;
+});
 
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
@@ -203,6 +390,5 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 function onYouTubeIframeAPIReady() {
-    console.log('init');
     init();
 }
